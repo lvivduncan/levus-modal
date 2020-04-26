a74a0b21e217bafed0b690584d503bdced3dbab6
lvivduncan <lvivduncan@gmail.com>
2020-04-26 15:19:30 +0300

    переписав на ново

--- js/levus-modal.js
+++ js/levus-modal.js
@@ -1,25 +1,30 @@
-/**
- * 
- * @param {*} el -- елемент, на якому клікають
- * @param {*} content -- блок, який має з'являтися по кліку
- */
-function levusModal(el,content){
+/*
+  2 блок:
+  1 -- елемент, на який клікають, 
+  2 -- контент, який має бути показано
+*/
+
+function levusModal(el,hidden){
+  // елемент, на який клікають
   const element = document.querySelector(el);
 
-  const div = document.createElement('div');
-  div.append(content);
-  div.setAttribute('style', 'position: absolute; top: calc(50% - 50px); right: calc(50% - 50px); bottom: calc(50% - 50px); left: calc(50% - 50px); width: 100px; height: 100px; background: red; color: white; padding: 10px;');
-
-  element.addEventListener('click', _ => {
-    document.body.appendChild(div);
-    document.body.style.backgroundColor = 'rgba(0,0,0,0.3)';
+  // контент, який має бути показаний після кліку
+  const content = document.querySelector(hidden);
+  
+  element.addEventListener('click', e => {
+    e.stopPropagation();
+    content.classList = 'levus-modal active';
+    document.body.classList = 'levus-body';
   });
 
-  setTimeout( _ => {
-    document.body.removeAttribute('style');
-    div.remove();
-  }, 3000);
+  document.body.addEventListener('click', _ => {
+    content.classList = 'levus-modal';
+    document.body.classList = '';
+  });
+
+  console.log(element, content)
 
 }
 
-levusModal('#button', 'test click');
+levusModal('#button', '#hidden');
+levusModal('#button2', '#hidden2');

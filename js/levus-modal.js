51ce482a3c8bd042eb071b502be7ebc7d6902999
lvivduncan <lvivduncan@gmail.com>
2020-05-13 14:37:51 +0300

    logic edit =)

--- js/levus-modal.js
+++ js/levus-modal.js
@@ -4,24 +4,35 @@
   2 -- контент, який має бути показано
 */
 
+// todo: кнопка "закрити" і закриття при натискання на "ескейп"
+
 function levusModal(el,hidden){
   // елемент, на який клікають
   const element = document.querySelector(el);
 
   // контент, який має бути показаний після кліку
   const content = document.querySelector(hidden);
+
+  // створюємо кнопку для закриття модального вікна
+  const close = document.createElement('div');
+  close.classList.add('levus-close');
+
+  document.addEventListener('DOMContentLoaded', _ => {
+    document.body.append(close);
+  });
   
   // відкриваємо вікно
-  element.addEventListener('click', e => {
-    e.stopPropagation();
+  element.addEventListener('click', _ => {
     content.classList = 'levus-modal active';
     document.body.classList = 'levus-body';
+    close.classList.add('active');
   });
 
   // закриваємо вікно
-  document.body.addEventListener('click', _ => {
+  close.addEventListener('click', _ => {
     content.classList = 'levus-modal';
     document.body.classList = '';
+    close.classList.remove('active');
   });
 
 }

1789c4a7026af4a70b05c0533ae26107399643c9
lvivduncan <lvivduncan@gmail.com>
2021-05-23 10:01:14 +0300

    version 2021 finish

--- js/levus-modal.js
+++ js/levus-modal.js
@@ -1,38 +1,42 @@
-const wrapper = document.createElement('div');
-wrapper.setAttribute('id', 'levus-wrapper');
+// 23-05-2021
+{
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('id', 'levus-wrapper');
 
-document.querySelectorAll('.levus-modal').forEach(item => {
-    item.addEventListener('click', function(e){
-        e.preventDefault();
+    document.querySelectorAll('.levus-modal').forEach(item => {
+        item.addEventListener('click', function(e){
+            e.preventDefault();
 
-        const link = this.dataset.link;
+            const link = this.dataset.link;
 
-        document.querySelectorAll('.levus-hidden-modal').forEach(elems => elems.classList.remove('active'));
+            document.querySelectorAll('.levus-hidden-modal').forEach(elems => elems.classList.remove('active'));
 
-        const viewport = window.innerHeight;
+            const viewport = window.innerHeight;
 
-        document.getElementById(link).classList.add('active');
-        document.getElementById(link).style.top = `${viewport/2-60}px`;
-        
-        // todo: window.innerHeight - document.documentElement.scrollHeight
-        document.querySelector('body').classList.add('lock');
-        document.querySelector('body').append(wrapper);
+            document.getElementById(link).classList.add('active');
+            document.getElementById(link).style.top = `${viewport/2-60}px`;
+            
+            document.querySelector('body').classList.add('lock');
+            document.querySelector('body').append(wrapper);
 
+        });
     });
-});
 
-wrapper.addEventListener('click', function(){
-    closeModal();
-});
-
-document.addEventListener('keydown', e => {
-    if(e.code == 'Escape' && e.key == 'Escape'){
+    wrapper.addEventListener('click', function(){
         closeModal();
-    }
-});
+    });
 
-function closeModal(){
-    document.querySelectorAll('.levus-hidden-modal').forEach(item => item.classList.remove('active'));
-    document.querySelector('body').classList.remove('lock');
-    wrapper.remove();
+    document.addEventListener('keydown', e => {
+        if(e.code == 'Escape' && e.key == 'Escape'){
+            closeModal();
+        }
+    });
+
+    function closeModal(){
+        document.querySelectorAll('.levus-hidden-modal').forEach(item => item.classList.remove('active'));
+        document.querySelector('body').classList.remove('lock');
+        wrapper.remove();
+    }
 }
+
+// todo: auto detect height and width modal size
